754b3e3479a576aacf70e67e66286766f8cf1405
Oscar Raizer <boltenkovs36@gmail.com>
2024-07-12 03:05:46 +0700

    feat: add animation for cards (add/delete)

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -27,15 +27,42 @@ const handleDeleteContact = (id: number) => {
     <div
         class="flex gap-5 items-center justify-center overflow-clip flex-wrap px-4"
     >
-        <ContactItem
-            v-for="item in items"
-            :key="item.id"
-            :id="item.id"
-            :name="item.name"
-            :mail="item.mail"
-            :phone="item.phone"
-            @update:contact="handleUpdateContact"
-            @delete:contact="handleDeleteContact"
-        />
+        <TransitionGroup name="contact">
+            <ContactItem
+                v-for="item in items"
+                :key="item.id"
+                :id="item.id"
+                :name="item.name"
+                :mail="item.mail"
+                :phone="item.phone"
+                @update:contact="handleUpdateContact"
+                @delete:contact="handleDeleteContact"
+            />
+        </TransitionGroup>
     </div>
 </template>
+<style>
+.contact-enter-active,
+.contact-leave-active {
+    animation: fadeInOut 0.3s ease forwards;
+}
+
+.contact-move {
+    transition: transform 0.3s ease;
+}
+
+@keyframes fadeInOut {
+    0% {
+        opacity: 0;
+        transform: translateY(30px);
+    }
+    100% {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+
+.contact-leave-active {
+    animation-direction: reverse;
+}
+</style>
